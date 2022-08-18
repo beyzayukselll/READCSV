e29c4ae8c094330b834175e3cc786b7b7879d43c
beyza <beyza.yuksel@bites.com.tr>
2022-08-18 11:44:34 +0300

    matplot

--- mil-cnc-min_log/mil-cnc-min/cnc-app/sim_math_data_types.h
+++
@@ -1,8 +0,0 @@
-#if !defined(SIM_MATH_DATA_TYPES_H)
-
-#include "sim_common.h"
-#include "sim_math_v3.h"
-#include "sim_math_mat3.h"
-
-#define SIM_MATH_DATA_TYPES_H
-#endif
--- mil-cnc-min_log/mil-cnc-min/cnc-app/sim_math_impl.hpp
+++
@@ -1,5 +0,0 @@
-// not exposed implementations
-
-#include "sim_common.h"
-#include "sim_math_v3.hpp"
-#include "sim_math_mat3.hpp"
